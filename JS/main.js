4ce48009a255982fe8a5015d44769418ab61f3e8
unknown <fogoste@gmail.com>
2022-05-03 09:46:52 -0300

    Salvando

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+function abrirDiscord(){
+    window.open('https://discord.gg/yJdxPwb79H', 'nova janela', 'width = 500px height = 500px;')
+}
+
+function abrirInstagram(){
+    window.open('https://www.instagram.com/accounts/login/?next=/guuh_raff/', 'nova janela', 'width = 500px height = 500px;')
+}
+
+function abrirGithub(){
+    window.open('https://www.github.com/Kyoudan', 'nova janela', 'width = 500px height = 500px;')
+}
